481b121508b9bdee55739448fb6d571109aab687
prsabahrami <pbahrami@uwaterloo.ca>
2024-08-30 00:44:21 -0400

    Change to localhost

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.APP_URL}/api/:path*`,
+                destination: `${process.env.APP_URL}:8000/api/:path*`,
             },
         ];
     },
