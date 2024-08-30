c76b059250d9fcdafce2f184123abfba60cd5fd8
prsabahrami <pbahrami@uwaterloo.ca>
2024-08-30 00:55:44 -0400

    wefwefw

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.APP_URL}/api/:path*`,
+                destination: `http://localhost:8000/api/:path*`,
             },
         ];
     },
